2d04615bb916837d10ac4434475199ac5bdab410
walk1ng <iliwgg@gmail.com>
2024-09-02 21:58:53 +0800

    fix jest due to app

--- server/node/src/__test__/index.test.js
+++ server/node/src/__test__/index.test.js
@@ -1,5 +1,5 @@
 const request = require('supertest');
-const app = require('../main');
+const app = require('../app');
 
 
 describe('GET /', () => {
--- server/node/src/app.js
+++ server/node/src/app.js
@@ -1,14 +1,8 @@
 const express = require('express');
 const app = express();
-const port = 4000;
-
-app.listen(port, () => {
-    console.log('Server is running on port', port);
-})
 
 const indexRouter = require('./routes/index');
 
 app.use('/', indexRouter);
-
 
 module.exports = app;
